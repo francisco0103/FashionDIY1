095e74248ad1e7e47915a20f2b65f8b8c0b4c582
Francisco Raí <franciscovieira0103@gmail.com>
2025-05-09 11:46:29 -0300

    git

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class index extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{ color: '#fff'}}>Login</Text>
+        <Text>Registre-se</Text>
+      </View>
+    )
+  }
+}
+
+export default index
